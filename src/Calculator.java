79ae8b2e6c9eeb61f499f7a117a4a58ca1192aa7
Leonid T <leonid.tahiraj@tutanota.com>
2021-10-08 20:22:36 +0200

    Calculator created with switch-case

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        int a,b,ergebnis;
 
+        System.out.println("Bitte die erste Zahl eingeben: ");
+        Scanner scanA = new Scanner(System.in);
+        a = scanA.nextInt();
+        System.out.println("Bitte die zweite Zahl eingeben: ");
+        Scanner scanB = new Scanner(System.in);
+        b = scanB.nextInt();
+
+        System.out.println("Möchten Sie addieren, subtrahieren, multiplizieren oder adieren? ");
+        Scanner scanCmd = new Scanner(System.in);
+        String cmd = scanCmd.next();
+
+
+            switch (cmd) {
+                case "+":
+                    ergebnis = a + b;
+                    System.out.println(a + " + " + b + " = " + ergebnis);
+                    break;
+                case "-":
+                    ergebnis = a + b;
+                    System.out.println(a + " + " + b + " = " + ergebnis);
+                    break;
+                case "*":
+                    ergebnis = a + b;
+                    System.out.println(a + " + " + b + " = " + ergebnis);
+                    break;
+                case "%":
+                    ergebnis = a + b;
+                    System.out.println(a + " + " + b + " = " + ergebnis);
+                    break;
+                default:
+                    break;
+            }
     }
 }
